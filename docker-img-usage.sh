28878e24bf16e7ad956b0fa26521129c7c2b28bf
zyphermonkey <zyphermonkey@gmail.com>
2019-03-29 15:21:49 -0400

    Update docker-img-usage.sh

--- docker-img-usage.sh
+++ docker-img-usage.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 docker_find_files () {
-  echo -e "\e[96m  Largest Files (+10M)\e[0m"
-  docker exec $1 sh -c '/usr/bin/find / -xdev $2 -path ./proc -prune -o -path ./sys -prune -o -type f -size +10000k -exec du -Shx {} + | sort -rh | head | sed "s/^/  /"'
+  echo -e "\e[96m  Largest Files (+50M)\e[0m"
+  docker exec $1 sh -c '/usr/bin/find / -xdev $2 -path ./proc -prune -o -path ./sys -prune -o -type f -size +50000k -exec du -Shx {} + | sort -rh | head | sed "s/^/    /"'
 }
 
 docker_find_directories () {
-  #du -hxa / --exclude="/data" --exclude="/dockerlogs" --exclude="/dockerlogs2" --exclude="/dockervarlib" --exclude="/license" --exclude="/opt/splunk/etc" --exclude="/opt/splunk/var" | sort -h -r | head
-  echo -e "\e[96m  Largest Directories\e[0m"
-  docker exec $1 sh -c 'du -hxa / $2 | sort -rh | head | sed "s/^/  /"'
+  echo -e "\e[96m  Largest Directories (+50M)\e[0m"
+  docker exec $1 sh -c 'du -hxa --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"'
 }
 
 docker_find_subdirectories () {
-  #du -Shx /  --exclude="/data" --exclude="/dockerlogs" --exclude="/dockerlogs2" --exclude="/dockervarlib" --exclude="/license" --exclude="/opt/splunk/etc" --exclude="/opt/splunk/var" | sort -rh | head
-  echo -e "\e[96m  Largest Directories (excluding subdirectories)\e[0m"
-  docker exec $1 sh -c 'du -Shx / $2 | sort -rh | head | sed "s/^/  /"'
+  echo -e "\e[96m  Largest Directories (+50M) (excluding subdirectories)\e[0m"
+  docker exec $1 sh -c 'du -Shx --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"'
 }
 
 docker_get_mounts () {
   mounts=$(docker inspect --format='{{range .Mounts}}{{json .Destination}}{{end}}' $1)
   #echo $mounts
-  
-  #echo $path
 }
 
 docker_set_exclude_path () {
@@ -31,7 +27,8 @@ docker_set_exclude_dir () {
   for i in `echo $mounts | tr \" '\n' | grep -v "^$"`; do exclude="$exclude --exclude='$i' "; done
   #echo $exclude
 }
-
+# Uncomment if you need, but leaving it commented out since it adds significant delay to the script. 
+# docker system df -v
 for container in $(docker ps --format '{{.Names}}' | sort -f)
 do 
   echo -e "\e[32;4m$container\e[0m"
@@ -39,6 +36,6 @@ do
   docker_set_exclude_path
   docker_set_exclude_dir
   docker_find_files $container $path
-  docker_find_directories $container $exclude
+  #docker_find_directories $container $exclude  #This needs works. Currently not useful
   docker_find_subdirectories $container $exclude
 done
