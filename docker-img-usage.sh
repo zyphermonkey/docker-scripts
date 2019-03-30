86ea2086798b52fc3fe20479ee315aaa2939b7f8
zyphermonkey <zyphermonkey@gmail.com>
2019-03-29 22:40:03 -0400

    only output if results per docker

--- docker-img-usage.sh
+++ docker-img-usage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 docker_find_files () {
-  echo -e "\e[96m  Largest Files (+50M)\e[0m"
-  docker exec $1 sh -c '/usr/bin/find / -xdev $2 -path ./proc -prune -o -path ./sys -prune -o -type f -size +50000k -exec du -Shx {} + | sort -rh | head | sed "s/^/    /"'
+  #echo -e "\e[96m  Largest Files (+50M)\e[0m"
+  docker_find_files_results=$(docker exec $1 sh -c '/usr/bin/find / -xdev $2 -path ./proc -prune -o -path ./sys -prune -o -type f -size +50000k -exec du -Shx {} + | sort -rh | head | sed "s/^/    /"')
 }
 
 docker_find_directories () {
-  echo -e "\e[96m  Largest Directories (+50M)\e[0m"
-  docker exec $1 sh -c 'du -hxa --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"'
+  #echo -e "\e[96m  Largest Directories (+50M)\e[0m"
+  docker_find_directories_results=$(docker exec $1 sh -c 'du -hxa --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"')
 }
 
 docker_find_subdirectories () {
-  echo -e "\e[96m  Largest Directories (+50M) (excluding subdirectories)\e[0m"
-  docker exec $1 sh -c 'du -Shx --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"'
+  #echo -e "\e[96m  Largest Directories (+50M) (excluding subdirectories)\e[0m"
+  docker_find_subdirectories_results=$(docker exec $1 sh -c 'du -Shx --threshold=50M / $2 | sort -rh | head | sed "s/^/    /"')
 }
 
 docker_get_mounts () {
@@ -27,15 +27,39 @@ docker_set_exclude_dir () {
   for i in `echo $mounts | tr \" '\n' | grep -v "^$"`; do exclude="$exclude --exclude='$i' "; done
   #echo $exclude
 }
+
+results () {
+  if [[ ! -z $docker_find_files_results || ! -z $docker_find_directories_results || ! -z $docker_find_subdirectories_results ]]; then
+    echo -e "\e[32;4m$1\e[0m"
+  fi 
+  
+  if [[ ! -z $docker_find_files_results ]]; then
+    echo -e "\e[96m  Largest Files (+50M)\e[0m"
+	echo $docker_find_files_results
+  fi 
+
+  if [[ ! -z $docker_find_directories_results ]]; then
+    echo -e "\e[96m  Largest Directories (+50M)\e[0m"
+	echo $docker_find_directories_results
+  fi 
+
+  if [[ ! -z $docker_find_subdirectories_results ]]; then
+    echo -e "\e[96m  Largest Directories (+50M) (excluding subdirectories)\e[0m"
+	echo $docker_find_subdirectories_results
+  fi 
+  
+}
+
 # Uncomment if you need, but leaving it commented out since it adds significant delay to the script. 
 # docker system df -v
 for container in $(docker ps --format '{{.Names}}' | sort -f)
 do 
-  echo -e "\e[32;4m$container\e[0m"
+  #echo -e "\e[32;4m$container\e[0m"
   docker_get_mounts "$container"
   docker_set_exclude_path
   docker_set_exclude_dir
   docker_find_files $container $path
   #docker_find_directories $container $exclude  #This needs works. Currently not useful
   docker_find_subdirectories $container $exclude
+  results $container $docker_find_files_results $docker_find_directories_results $docker_find_subdirectories_results
 done
